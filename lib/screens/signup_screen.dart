ae39e6e9826233c1942ad700ff1684c26b26c23b
Vaibhav Jaiswal <vaibhav.jaiswal.vj91@gmail.com>
2022-05-02 17:07:13 +0530

    Login_screen_completed

--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'dart:ui';
 
@@ -7,22 +7,26 @@ import 'package:flutter_svg/flutter_svg.dart';
 import 'package:instagram_clone/utils/colors.dart';
 import 'package:instagram_clone/widget/text_field_input.dart';
 
-class LoginScreen extends StatefulWidget {
-  const LoginScreen({Key? key}) : super(key: key);
+class SignupScreen extends StatefulWidget {
+  const SignupScreen({Key? key}) : super(key: key);
 
   @override
-  State<LoginScreen> createState() => _LoginScreenState();
+  State<SignupScreen> createState() => _SignupScreenState();
 }
 
-class _LoginScreenState extends State<LoginScreen> {
+class _SignupScreenState extends State<SignupScreen> {
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _bioController = TextEditingController();
+  final TextEditingController _usernameController = TextEditingController();
   @override
   void dispose() {
     // TODO: implement dispose
     super.dispose();
     _emailController.dispose();
     _passwordController.dispose();
+    _bioController.dispose();
+    _usernameController.dispose();
   }
 
   @override
@@ -44,6 +48,21 @@ class _LoginScreenState extends State<LoginScreen> {
               const SizedBox(
                 height: 64,
               ),
+              Stack(
+                children: [
+                  CircleAvatar(
+                    
+                    backgroundImage: NetworkImage("https://unsplash.com/photos/Kj2SaNHG-hg"),)
+                ],
+              ),
+              TextFieldInput(
+                hintText: 'Enter Your Username',
+                textInputType: TextInputType.text,
+                textEditingController: _usernameController,
+              ),
+               const SizedBox(
+                height: 44,
+              ),
               TextFieldInput(
                 hintText: 'Enter Your Email',
                 textInputType: TextInputType.emailAddress,
@@ -52,6 +71,7 @@ class _LoginScreenState extends State<LoginScreen> {
               const SizedBox(
                 height: 24,
               ),
+              
               TextFieldInput(
                 hintText: 'Enter Your Password',
                 textInputType: TextInputType.text,
@@ -61,11 +81,16 @@ class _LoginScreenState extends State<LoginScreen> {
               const SizedBox(
                 height: 24,
               ),
+              TextFieldInput(
+                hintText: 'Enter Your bio',
+                textInputType: TextInputType.text,
+                textEditingController: _bioController,
+              ),
               InkWell(
                 child: Container(
                   child: !_isLoading
                       ? const Text(
-                          'Log in',
+                          'Sign up',
                         )
                       : const CircularProgressIndicator(
                           color: primaryColor,
@@ -80,7 +105,7 @@ class _LoginScreenState extends State<LoginScreen> {
                     color: blueColor,
                   ),
                 ),
-                onTap: loginUser,
+                onTap: signUpUser,
               ),
               const SizedBox(
                 height: 12,
@@ -96,9 +121,7 @@ class _LoginScreenState extends State<LoginScreen> {
                     ),
                   ),
                   GestureDetector(
-                    onTap: () {
-                      
-                    },
+                    onTap: () {},
                     child: Container(
                       child: const Text(
                         "Sign Up",
